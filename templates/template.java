16ea210ffa331b002ee8d49a2c768332ea01deef
Francis <antiphysika@gmail.com>
2024-07-08 11:33:36 +0100

    Add templates/

---
+++ templates/template.java
@@ -0,0 +1,18 @@
+// Project Initium
+//
+// File:
+//   projectinitium:/<PATH>
+//
+// Copyright:
+//   Copyright (c) 2004 Francois <antiphysika@gmail.com>
+//
+// Authors:
+//   Francois aka Anti-Physika
+//
+// SPDX-License-Identifier: LGPL-2.1
+//
+
+
+
+//
+// vim: ts=2 sw=2 et fdm=marker :
